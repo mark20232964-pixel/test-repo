a7738a7514028354a4be3d9da700059c7f1b7967
Imethma Methusari Yapa Bandara <imy@Imethmas-MacBook-Air.local>
2026-03-14 16:27:53 +0530

    feat(user-profile): add AppBar with back button and Profile title

--- lib/screens/user/user_profile_screen.dart
+++ lib/screens/user/user_profile_screen.dart
@@ -8,6 +8,20 @@ class UserProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
+      appBar: AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
+        onPressed: () => Navigator.pop(context),
+      ),
+      title: const Text(
+        "Profile",
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+    ),
       body: const Center(
         child: Text('User Profile - Step 1', style: TextStyle(fontSize: 28)),
       ),
