1cddba9a58d1c3b32fc04dca79a39f5ef38ff469
uvini-wickramaarachchi <uvini.20232398@iit.ac.lk>
2026-03-15 18:45:01 +0530

    feat(provider): create empty AddGarageScreen skeleton

---
+++ lib/screens/provider/add_garage_screen.dart
@@ -0,0 +1,19 @@
+// lib/screens/provider/add_garage_screen.dart
+
+import 'package:flutter/material.dart';
+
+class AddGarageScreen extends StatelessWidget {
+  const AddGarageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a Garage'),
+      ),
+      body: const Center(
+        child: Text('Add Garage screen - coming soon'),
+      ),
+    );
+  }
+}
