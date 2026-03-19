fd75c8d8814b03b87970b2bbe4419b71143cb742
uvini-wickramaarachchi <uvini.20232398@iit.ac.lk>
2026-03-20 02:47:17 +0530

    feat: add form key and text controllers

--- lib/screens/provider/provider_add_new_garage_screen.dart
+++ lib/screens/provider/provider_add_new_garage_screen.dart
@@ -8,6 +8,25 @@ class AddGarageScreen extends StatefulWidget {
 }
 
 class _AddGarageScreenState extends State<AddGarageScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _contactController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _addressController.dispose();
+    _contactController.dispose();
+    _emailController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
