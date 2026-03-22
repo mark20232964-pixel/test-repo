c9c34d393e9c31ffacb0b4b3c374f8cbcb1448a8
Danel <mark.20232964@iit.ac.lk>
2026-03-22 17:19:06 +0530

    feat: create empty AddMechanicScreen file
    - Added initial file in provider folder for adding mechanic details
    - Used StatefulWidget base (needed for map/location state later)
    - Placeholder widget to visualize empty screen

---
+++ lib/screens/provider/add_mechanic.dart
@@ -0,0 +1,17 @@
+// lib/screens/provider/add_mechanic.dart
+
+import 'package:flutter/material.dart';
+
+class AddMechanicScreen extends StatefulWidget {
+  const AddMechanicScreen({super.key});
+
+  @override
+  State<AddMechanicScreen> createState() => _AddMechanicScreenState();
+}
+
+class _AddMechanicScreenState extends State<AddMechanicScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
