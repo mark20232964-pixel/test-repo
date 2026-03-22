a849434dd61a5f8baab720de994e6acc8ffd7fcc
Danel <mark.20232964@iit.ac.lk>
2026-03-22 15:59:31 +0530

    feat: convert AddServiceTypeScreen to StatefulWidget
    - Changed from StatelessWidget to StatefulWidget for future state (controllers, loading, etc.)
    - Added createState() and private _AddServiceTypeScreenState class
    - Prepared for dynamic UI and user interactions in next commits

--- lib/screens/provider/add_service_type.dart
+++ lib/screens/provider/add_service_type.dart
@@ -2,9 +2,14 @@
 
 import 'package:flutter/material.dart';
 
-class AddServiceTypeScreen extends StatelessWidget {
+class AddServiceTypeScreen extends StatefulWidget {
   const AddServiceTypeScreen({super.key});
 
+  @override
+  State<AddServiceTypeScreen> createState() => _AddServiceTypeScreenState();
+}
+
+class _AddServiceTypeScreenState extends State<AddServiceTypeScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder(); // temporary - we'll build UI step by step
