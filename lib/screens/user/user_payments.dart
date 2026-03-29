9e0e15a4c338f4246a2370283606e9a588c27ac9
hasithe5miling <hasithe5miling@gmail.com>
2026-03-29 20:58:01 +0530

    feat: add proper date formatting using intl package

--- lib/screens/user/user_payments.dart
+++ lib/screens/user/user_payments.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class AppPaymentsScreen extends StatefulWidget {
   const AppPaymentsScreen({super.key});
